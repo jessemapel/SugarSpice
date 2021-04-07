ec426c995a9a202d01daf6c328135c49960cbcc0
Kelvin <kelvinrr@icloud.com>
2021-04-06 23:26:13 -0700

    added to env deps

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ "breathe", "m2r2" ]
+extensions = [ "breathe", "m2r2", "sphinx-material" ]
 
 # Breathe Configuration
 breathe_default_project = "SugarSpice"
