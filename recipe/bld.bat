ed895adcd3d7459a7838f0d1036c1c9e0f951f6d
Jesse Mapel <jmapel@usgs.gov>
2021-12-03 09:44:10 -0700

    Updated wrapper output and install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,5 +3,4 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=$PREFIX -DSPICEQL_BUILD_DOCS=OFF -DSPICEQL_BUILD_TESTS=OFF ..
 cmake --build . --config RELEASE
 cmake --install . --config RELEASE
-cmake --build . --target install-python
 if errorlevel 1 exit 1
